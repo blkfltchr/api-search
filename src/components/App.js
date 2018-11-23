5c3b3661780de6c7722ef0cd5e5f7f2b92607bdb
Blake Fletcher <blkfltchr@gmail.com>
2018-11-23 14:47:30 -0500

    Searchbars rendering from SearchBar.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {Route, Link} from 'react-router-dom'
 import ImageSearch from './imagesearch/ImageSearch'
 import VideoSearch from './videosearch/VideoSearch'
+import SearchBar from './SearchBar'
 
 class App extends React.Component {
     state = {
-        searchType: ''
+        searchType: 'all'
     }
 
     render() {
@@ -13,10 +14,11 @@ class App extends React.Component {
             <div>
                 <div className="ui container">
                     <div className="ui segment">
-                        <Link to="/">Home</Link>    
+                        <Link onClick={e => this.setState({searchType: 'all'})} to="/">Home</Link>    
                         <Link onClick={e => this.setState({searchType: 'image'})} to="/images">Images</Link>
                         <Link onClick={e => this.setState({searchType: 'video'})} to="/videos">Videos</Link>
                     </div>
+                        <SearchBar searchType={this.state.searchType}/>
                 </div>
                 <Route exact path="/images" component={ImageSearch}/>
                 <Route exact path="/videos" component={VideoSearch}/>
---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import ImageSearchBar from './imagesearch/ImageSearchBar'
+import VideoSearchBar from './videosearch/VideoSearchBar'
+
+class SearchBar extends React.Component {
+
+    render() {
+        if (this.props.searchType === 'all') {
+            return (
+                <div className="ui segment">
+                    <form className="ui form">
+                        <div className="ui icon input" style={{width: "100%"}}>
+                            <input style={{borderRadius:"15px"}} 
+                                type="text" 
+                                placeholder="Search..." 
+                            />
+                            <i className="search icon"></i>
+                        </div>
+                    </form>
+                </div>
+            );
+        } else if (this.props.searchType === 'image') {
+            return(
+                <ImageSearchBar />
+            )
+        } else if (this.props.searchType === 'video') {
+            return (
+                <VideoSearchBar />
+            )
+        }
+    }
+}
+
+export default SearchBar
