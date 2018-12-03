a53ca0a59643c582bd0321a350d5421e014b8749
Blake Fletcher <blkfltchr@gmail.com>
2018-12-02 20:55:00 -0800

    Searchbar.js CSS clean up

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,41 +3,28 @@ import {NavLink} from 'react-router-dom';
 class SearchBar extends React.Component {
 
     render() {
-        // if (this.props.searchType === 'all') {
-            return (
-
-            <div className="ui"  style={{marginBottom:"1rem"}}>
-                    <div style={{backgroundColor: "#F6F9FC", borderBottom: "solid 1px lightgrey"}}>
-                    
-                <div className="ui container" style={{paddingTop: "1rem"}}>
-                    <form className="ui form">
-                        <div className="ui icon input" style={{width: "100%"}}>
-                            <input style={{borderRadius:"15px"}} 
-                                type="text" 
-                                placeholder="Search..." 
-                            />
-                            <i className="search icon"></i>
-                        </div>
-                    </form>
-                </div>
-                        <div className="ui nav-wrapper">
-                            <NavLink exact onClick={e => this.setState({searchType: 'all'})} activeStyle={{ textDecoration: 'underline' }} to="/" className="nav-element">Home</NavLink>    
-                            <NavLink onClick={e => this.setState({searchType: 'image'})} activeStyle={{ textDecoration: 'underline' }} to="/images" className="nav-element">Images</NavLink>
-                            <NavLink onClick={e => this.setState({searchType: 'video'})} activeStyle={{ textDecoration: 'underline' }} to="/videos" className="nav-element">Videos</NavLink>
-                        </div>
+        return (
+            <div className="ui searchbar-wrapper">
+                <div className="searchbar-background">    
+                    <div className="ui container" style={{paddingTop: "1rem"}}>
+                        <form className="ui form">
+                            <div className="ui icon input" style={{width: "100%"}}>
+                                <input className="searchbar-icon" 
+                                    type="text" 
+                                    placeholder="Search..." 
+                                />
+                                <i className="search icon"></i>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="ui nav-wrapper">
+                        <NavLink exact onClick={e => this.setState({searchType: 'all'})} activeStyle={{ textDecoration: 'underline' }} to="/" className="nav-element">Home</NavLink>    
+                        <NavLink onClick={e => this.setState({searchType: 'image'})} activeStyle={{ textDecoration: 'underline' }} to="/images" className="nav-element">Images</NavLink>
+                        <NavLink onClick={e => this.setState({searchType: 'video'})} activeStyle={{ textDecoration: 'underline' }} to="/videos" className="nav-element">Videos</NavLink>
                     </div>
                 </div>
-
-            );
-        // } else if (this.props.searchType === 'image') {
-        //     return(
-        //         <ImageSearchBar />
-        //     )
-        // } else if (this.props.searchType === 'video') {
-        //     return (
-        //         <VideoSearchBar />
-        //     )
-        // }
+            </div>
+        );
     }
 }
 
